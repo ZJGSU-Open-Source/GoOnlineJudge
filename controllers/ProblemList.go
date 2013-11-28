aaff8d6814bad05c5f259e7efc6ec9faaf9cf92d
memelee <Meme@Call-Me-Mac.local>
2013-11-28 15:45:30 +0800

    Feature

--- controllers/ProblemList.go
+++ controllers/ProblemList.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"GoOnlineJudge/config"
 	"html/template"
 	"log"
 	"net/http"
@@ -12,7 +13,7 @@ type ProblemListController struct {
 func (this *ProblemListController) GET(w http.ResponseWriter, r *http.Request) {
 	log.Println("Problem List")
 
-	t, err := template.ParseFiles("views/problemlist.tpl", "views/head.tpl", "views/foot.tpl")
+	t, err := this.getTemplate()
 	if err != nil {
 		log.Println(err)
 	}
@@ -20,4 +21,12 @@ func (this *ProblemListController) GET(w http.ResponseWriter, r *http.Request) {
 	data := &Data{}
 	data.Title = "Problem List"
 	t.Execute(w, data)
+}
+
+func (this *ProblemListController) getTemplate() (*template.Template, error) {
+	if config.Problem {
+		return template.ParseFiles("views/problemlist.tpl", "views/head.tpl", "views/foot.tpl")
+	} else {
+		return template.ParseFiles("views/close.tpl", "views/head.tpl", "views/foot.tpl")
+	}
 }
