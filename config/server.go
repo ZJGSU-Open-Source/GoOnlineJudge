72ede844cd4d7c58390247a3590b311d12334284
JinweiClarkChao <clarkzjw@gmail.com>
2014-07-31 19:13:32 +0800

    datapath为相对路径时，添加问题会有create dir error, 但实际上问题已经添加，定位在/admin/problem.go line 212

--- config/server.go
+++ config/server.go
@@ -2,4 +2,4 @@ package config
 
 const PostHost = "http://127.0.0.1:8888"
 const CookieExpires = 1800
-const Datapath = "~/Data"
+const Datapath = "/home/jinwei/ProblemData/" //Datapath 为相对路径时，添加问题会有create dir error, 但实际上问题已经添加，定位在/admin/problem.go line 212
