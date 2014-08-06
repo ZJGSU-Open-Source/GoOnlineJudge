d9418aa170a5ee25f932c836a44c6c2c6589750c
sakeven <jc5930@sina.cn>
2014-08-07 00:42:01 +0800

    性能优化

--- controller/help.go
+++ controller/help.go
@@ -2,7 +2,6 @@ package controller
 
 import (
 	"GoOnlineJudge/class"
-	"GoOnlineJudge/config"
 	"html/template"
 	"log"
 	"net/http"
@@ -16,14 +15,6 @@ func (this *HelpController) Help(w http.ResponseWriter, r *http.Request) {
 	log.Println("Help Page")
 	this.Init(w, r)
 
-	response, err := http.Post(config.PostHost+"/help", "application/json", nil)
-	if err != nil {
-		http.Error(w, "post error", 500)
-		return
-	}
-	defer response.Body.Close()
-
-	//t := template.New("layout.tpl")
 	t, err := template.ParseFiles("view/layout.tpl", "view/help.tpl")
 	if err != nil {
 		http.Error(w, "tpl error", 500)
