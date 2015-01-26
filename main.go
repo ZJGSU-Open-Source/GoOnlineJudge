40ebcd3216e087b84ab3050fa2ccb846dab3d1f3
sakeven <jc5930@sina.cn>
2015-01-26 20:57:15 +0800

    Add HDU_VJudger

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"log"
+	"restweb"
+
+	"GoOnlineJudge/controller"
+	"GoOnlineJudge/controller/admin"
+	"GoOnlineJudge/controller/contest"
+)
+
+func main() {
+
+	restweb.RegisterController(&controller.FAQController{})
+	restweb.RegisterController(&controller.HomeController{})
+	restweb.RegisterController(&controller.NewsController{})
+	restweb.RegisterController(&controller.RanklistController{})
+	restweb.RegisterController(&controller.SessController{})
+	restweb.RegisterController(&controller.UserController{})
+	restweb.RegisterController(&controller.ContestController{})
+	restweb.RegisterController(&controller.StatusController{})
+	restweb.RegisterController(&controller.OSCController{})
+	restweb.RegisterController(&controller.ProblemController{})
+	restweb.RegisterController(&admin.AdminNotice{})
+	restweb.RegisterController(&admin.AdminProblem{})
+	restweb.RegisterController(&admin.AdminRejudge{})
+	restweb.RegisterController(&admin.AdminTestdata{})
+	restweb.RegisterController(&admin.AdminContest{})
+	restweb.RegisterController(&admin.AdminHome{})
+	restweb.RegisterController(&admin.AdminUser{})
+	restweb.RegisterController(&admin.AdminImage{})
+	restweb.RegisterController(&admin.AdminNews{})
+	restweb.RegisterController(&contest.ContestProblem{})
+	restweb.RegisterController(&contest.ContestRanklist{})
+	restweb.RegisterController(&contest.ContestStatus{})
+	restweb.RegisterController(&contest.Contest{})
+
+	restweb.AddFile("/static/", ".")
+	log.Fatal(restweb.Run())
+}
