c1da1b35109c4bacfdb448c4f38a27930b149726
sakeven <jc5930@sina.cn>
2015-01-27 19:14:25 +0800

    vj

--- controller/vjudger/hduoj.go
+++ controller/vjudger/hduoj.go
@@ -14,19 +14,41 @@ import (
 )
 
 type HDUJudger struct {
-	client *http.Client
-	token  string
-	pat    *regexp.Regexp
+	client   *http.Client
+	token    string
+	pat      *regexp.Regexp
+	username string
+	userpass string
 }
 
-var HDURes = map[string]int{"Accepted": 1}
+var HDURes = map[string]int{"Queuing": 0,
+	"Compiling": 1, "Running": 1,
+	"Compilation Error":                         2,
+	"Accepted":                                  3,
+	"Runtime Error<br>(STACK_OVERFLOW)":         4,
+	"Runtime Error<br>(ACCESS_VIOLATION)":       4,
+	"Runtime Error<br>(ARRAY_BOUNDS_EXCEEDED)":  4,
+	"Runtime Error<br>(FLOAT_DENORMAL_OPERAND)": 4,
+	"Runtime Error<br>(FLOAT_DIVIDE_BY_ZERO)":   4,
+	"Runtime Error<br>(FLOAT_OVERFLOW)":         4,
+	"Runtime Error<br>(FLOAT_UNDERFLOW )":       4,
+	"Runtime Error<br>(INTEGER_OVERFLOW)":       4,
+	"Runtime Error<br>(INTEGER_DIVIDE_BY_ZERO)": 4,
+	"Wrong Answer":                              5,
+	"Time Limit Exceeded":                       6,
+	"Memory Limit Exceeded":                     7,
+	"Output Limit Exceeded":                     8,
+	"Presentation Error":                        9,
+	"System Error":                              10}
 
 func (h *HDUJudger) Init(_ *User) error {
 	jar, _ := cookiejar.New(nil)
 	h.client = &http.Client{Jar: jar}
 	h.token = "HDU"
-	pattern := `\d+</td><td>(.*?)</td><td><font color=.*?>(.*?)</font>.*?<td>(\d+)MS</td><td>(\d+)K</td><td><a href="/viewcode.php\?rid=\d+"  target=_blank>(\d+) B</td><td>(.*?)</td>`
+	pattern := `(\d+)</td><td>(.*?)</td><td>(?s:.*?)<font color=.*?>(.*?)</font>.*?<td>(\d+)MS</td><td>(\d+)K</td><td><a href="/viewcode.php\?rid=\d+"  target=_blank>(\d+) B</td><td>(.*?)</td>`
 	h.pat = regexp.MustCompile(pattern)
+	h.username = "mysake"
+	h.userpass = "123456"
 	return nil
 }
 
@@ -40,11 +62,9 @@ func (h *HDUJudger) Login(_ *User) error {
 
 	h.client.Get("http://acm.hdu.edu.cn")
 
-	username := "mysake"
-	userpass := "123456"
 	uv := url.Values{}
-	uv.Add("username", username)
-	uv.Add("userpass", userpass)
+	uv.Add("username", h.username)
+	uv.Add("userpass", h.userpass)
 	uv.Add("login", "Sign In")
 
 	req, err := http.NewRequest("POST", "http://acm.hdu.edu.cn/userloginex.php?action=login", strings.NewReader(uv.Encode()))
@@ -63,18 +83,13 @@ func (h *HDUJudger) Login(_ *User) error {
 	}
 	defer resp.Body.Close()
 
-	//TODO check login
-	return nil
-	// b, _ := ioutil.ReadAll(resp.Body)
-	// log.Println(resp.Header)
-	// log.Println(resp.Status)
+	b, _ := ioutil.ReadAll(resp.Body)
+	html := string(b)
+	if strings.Index(html, "No such user or wrong password.") >= 0 {
+		return LoginFailed
+	}
 
-	// f, err := os.Create("/Users/sake/hdu.html")
-	// if err != nil {
-	// 	return
-	// }
-	// defer f.Close()
-	// f.Write(b)
+	return nil
 }
 
 func (h *HDUJudger) Submit(u *User) error {
@@ -103,50 +118,92 @@ func (h *HDUJudger) Submit(u *User) error {
 	}
 	defer resp.Body.Close()
 
+	b, _ := ioutil.ReadAll(resp.Body)
+	html := string(b)
+	if strings.Index(html, "No such problem") >= 0 {
+		return NoSuchProblem
+	}
+	if strings.Index(html, "One or more following ERROR(s) occurred.") >= 0 {
+		return SubmitFailed
+	}
 	//TODO check whether submit success
 	return nil
 }
 
 func (h *HDUJudger) GetStatus(u *User) error {
-	time.Sleep(1 * time.Second)
-	username := "mysake"
+
 	statusUrl := "http://acm.hdu.edu.cn/status.php?first=&" +
 		"pid=" + strconv.Itoa(u.Vid) +
-		"&user=" + username +
+		"&user=" + h.username +
 		"&lang=" + strconv.Itoa(u.Lang) + "&status=0"
-	resp, err := h.client.Get(statusUrl)
+
+	endTime := time.Now().Add(MAX_WaitTime * time.Second)
+
+	for true {
+		if time.Now().After(endTime) {
+			return BadStatus
+		}
+		resp, err := h.client.Get(statusUrl)
+		if err != nil {
+			log.Println(err)
+			return BadInternet
+		}
+		defer resp.Body.Close()
+
+		b, _ := ioutil.ReadAll(resp.Body)
+		AllStatus := h.pat.FindAllStringSubmatch(string(b), -1)
+
+		layout := "2006-01-02 15:04:05 (MST)" //parse time
+		for i := len(AllStatus) - 1; i >= 0; i-- {
+			status := AllStatus[i]
+			t, _ := time.Parse(layout, status[2]+" (CST)")
+			t = t.Add(40 * time.Second) //HDU server's time is less 36s.
+			log.Println(t, u.SubmitTime)
+			log.Println(status[1:])
+			if t.After(u.SubmitTime) {
+				rid := status[1]
+				u.Result = HDURes[status[3]]
+
+				if u.Result >= 2 {
+					if u.Result == 2 {
+						u.CE, err = h.GetCEInfo(rid)
+						if err != nil {
+							log.Println(err)
+						}
+					}
+
+					u.Time, _ = strconv.Atoi(status[4])
+					u.Mem, _ = strconv.Atoi(status[5])
+					u.Length, _ = strconv.Atoi(status[6])
+					return nil
+				}
+			}
+		}
+		time.Sleep(1 * time.Second)
+	}
+	log.Println("here")
+	return nil
+}
+
+func (h *HDUJudger) GetCEInfo(rid string) (string, error) {
+	resp, err := h.client.Get("http://acm.hdu.edu.cn/viewerror.php?rid=" + rid)
 	if err != nil {
 		log.Println(err)
-		return BadInternet
+		return "", BadInternet
 	}
-	defer resp.Body.Close()
 
 	b, _ := ioutil.ReadAll(resp.Body)
-	s := string(b)
-	find := h.pat.FindAllStringSubmatch(s, -1)
-
-	layout := "2006-01-02 15:04:05 (MST)" //parse time
-
-	for i := len(find) - 1; i >= 0; i-- {
-		str := find[i]
-		t, _ := time.Parse(layout, str[1]+" (CST)")
-		t = t.Add(36 * time.Second) //HDU server's time is less 36s.
-		if t.After(u.SubmitTime) {
-			log.Println(str[1:])
-			u.Result = HDURes[str[2]]
-			u.Time, _ = strconv.Atoi(str[3])
-			u.Mem, _ = strconv.Atoi(str[4])
-			u.Length, _ = strconv.Atoi(str[5])
-			break
-		}
-	}
-	return nil
+	pre := "(?s)<pre>(.*?)</pre>"
+	re := regexp.MustCompile(pre)
+	match := re.FindStringSubmatch(string(b))
+	return match[1], nil
 }
 
 func (h *HDUJudger) Run(u *User) error {
 	for _, apply := range []func(*User) error{h.Init, h.Login, h.Submit, h.GetStatus} {
 		t := time.Now()
 		if err := apply(u); err != nil {
+			log.Println(err)
 			return err
 		}
 		log.Println("time:", time.Now().Sub(t))
