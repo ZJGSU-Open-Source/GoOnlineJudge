7cb32e822777f71a71aacf27c1ceb7349285ebea
sakeven <jc5930@sina.cn>
2015-02-06 20:38:23 +0800

    PKU importor

--- schedule/init.go
+++ schedule/init.go
@@ -15,4 +15,14 @@ func init() {
 	hdu := &HDUJudger{}
 	hdu.Init()
 	go hdu.GetProblems()
+
+	PKULogfile, err := os.Create("log/pku.log")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	PKUlogger = log.New(PKULogfile, "[PKU]", log.Ldate|log.Ltime)
+	PKU := &PKUJudger{}
+	PKU.Init()
+	go PKU.GetProblems()
 }
