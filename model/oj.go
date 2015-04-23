9a2e0a3e8befb37baf7dfee32fe66ecabce34b28
sakeven <jc5930@sina.cn>
2015-04-24 00:43:55 +0800

    support voj status check

---
+++ model/oj.go
@@ -0,0 +1,65 @@
+package model
+
+import (
+    "GoOnlineJudge/model/class"
+    "gopkg.in/mgo.v2"
+    "gopkg.in/mgo.v2/bson"
+)
+
+type OJStatus struct {
+    Name   string `json:"name"bson:"name"`
+    Status int    `json:"status"bson:"status"`
+}
+
+type OJModel struct {
+    class.Model
+}
+
+var statusListSelector = bson.M{"_id": 0,
+    "name":   1,
+    "status": 1,
+}
+
+func (o *OJModel) Update(status *OJStatus) error {
+
+    if status == nil {
+        return ArgsErr
+    }
+    err := o.OpenDB()
+    if err != nil {
+        return DBErr
+    }
+    defer o.CloseDB()
+
+    alt := make(map[string]interface{})
+    alt["status"] = status.Status
+
+    err = o.DB.C("OJStatus").Update(bson.M{"name": status.Name}, bson.M{"$set": alt})
+    if err == mgo.ErrNotFound {
+        err = o.DB.C("OJStatus").Insert(status)
+        if err != nil {
+            return OpErr
+        }
+    } else if err != nil {
+        return OpErr
+    }
+
+    return nil
+}
+
+func (o *OJModel) List() ([]*OJStatus, error) {
+    err := o.OpenDB()
+    if err != nil {
+        return nil, DBErr
+    }
+    defer o.CloseDB()
+
+    q := o.DB.C("OJStatus").Find(nil).Select(statusListSelector).Sort("name")
+
+    var list []*OJStatus
+    err = q.All(&list)
+    if err != nil {
+        return nil, OpErr
+    }
+    return list, nil
+}
--- view/news_list.tpl
+++ view/news_list.tpl
@@ -1,5 +1,8 @@
 {{define "content"}}
+<div class="row">
+
 <h2 class="page-header">News List</h2>
+<div class="col-md-8">
 {{with .News}}
 
 	{{range .}}
@@ -13,4 +16,31 @@
 		{{end}}
 	{{end}}
 {{end}}
+</div>
+<div class="col-md-4">
+	<table id="problem_list" class="table table-bordered table-striped table-hover">
+  <thead>
+    <tr>
+      <th class="header">OJ</th>
+      <th class="header">STATUS</th>
+    </tr>
+  </thead>
+  <tbody>
+  {{with .OJStatus}}
+  	{{range .}}
+    <tr>
+      <td>{{.Name}}</td>
+      <td>{{if eq .Status 0}} 
+      		<span class="submitRes-3"><strong>ok</strong></span>
+      		{{else}}
+      		<span class="submitRes-4"> <strong>Unavailable</strong></span>
+      		{{end}}
+      </td>
+    </tr>
+    {{end}}
+   {{end}}
+  </tbody>
+</table>
+</div>
+</div>
 {{end}}
