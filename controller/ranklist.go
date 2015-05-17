7d7325d1cb4451f9f1e225020c600c01524ad942
sakeven <jc5930@sina.cn>
2015-05-17 20:16:45 +0800

    ranklist & status api

--- controller/ranklist.go
+++ controller/ranklist.go
@@ -1,86 +1,57 @@
 package controller
 
 import (
-	"GoOnlineJudge/class"
-	"GoOnlineJudge/config"
-	"GoOnlineJudge/model"
-	"net/http"
-	"restweb"
-	"strconv"
+    "GoOnlineJudge/class"
+    "GoOnlineJudge/config"
+    "GoOnlineJudge/model"
+
+    "restweb"
+
+    "encoding/json"
+    "net/http"
 )
 
 // 排名
 type rank struct {
-	model.User
-	Index int `json:"index"bson:"index"`
+    model.User
+    Index int `json:"index"bson:"index"`
 }
 
 // 排名控件
 type RanklistController struct {
-	class.Controller
-} //@Controller
+    class.Controller
+}   //@Controller
 
-//@URL: /ranklist @method: GET
+//@URL: /api/ranklist @method: GET
 func (rc *RanklistController) Index() {
-	restweb.Logger.Debug("Ranklist")
+    restweb.Logger.Debug("Ranklist")
 
-	// Page
+    in := struct {
+        Offset int
+        Limit  int
+    }{}
+    if err := json.NewDecoder(rc.R.Body).Decode(&in); err != nil {
+        rc.Error(err.Error(), http.StatusBadRequest)
+        return
+    }
+    userModel := model.UserModel{}
 
-	if _, ok := rc.Input["page"]; !ok {
-		rc.Input.Set("page", "1")
-	}
+    qry := make(map[string]string)
 
-	userModel := model.UserModel{}
-	userList, err := userModel.List(nil)
-	if err != nil {
-		http.Error(rc.W, err.Error(), 400)
-		return
-	}
+    userList, err := userModel.List(qry)
+    if err != nil {
 
-	var count int
-	count = 1
-	for _, one := range userList {
-		if one.Status == config.StatusAvailable {
-			count += 1
-		}
-	}
+    }
 
-	var pageCount = (count-1)/config.UserPerPage + 1
-	page, err := strconv.Atoi(rc.Input.Get("page"))
-	if err != nil {
-		rc.Error("args error", 400)
-		return
-	}
-	if page > pageCount {
-		rc.Error("args error", 400)
-		return
-	}
-
-	pageData := rc.GetPage(page, pageCount)
-	for k, v := range pageData {
-		rc.Output[k] = v
-	}
-
-	qry := make(map[string]string)
-	qry["offset"] = strconv.Itoa((page - 1) * config.UserPerPage)
-	qry["limit"] = strconv.Itoa(config.UserPerPage)
-	userList, err = userModel.List(qry)
-	if err != nil {
-
-	}
-
-	list := make([]rank, len(userList), len(userList))
-	count = 1
-	for i, one := range userList {
-		list[i].User = *one
-		if one.Status == config.StatusAvailable {
-			list[count-1].Index = count + (page-1)*config.UserPerPage
-			count += 1
-		}
-	}
-	rc.Output["URL"] = "/ranklist?"
-	rc.Output["User"] = list
-	rc.Output["Title"] = "Ranklist"
-	rc.Output["IsRanklist"] = true
-	rc.RenderTemplate("view/layout.tpl", "view/ranklist.tpl")
+    list := make([]rank, len(userList), len(userList))
+    count := 1
+    for i, one := range userList {
+        list[i].User = *one
+        if one.Status == config.StatusAvailable {
+            list[count-1].Index = count + in.Offset*in.Limit
+            count += 1
+        }
+    }
+    rc.Output["User"] = list
+    rc.RenderJson()
 }
--- controller/status.go
+++ controller/status.go
@@ -1,122 +1,70 @@
 package controller
 
 import (
-	"GoOnlineJudge/class"
-	"GoOnlineJudge/config"
-	"GoOnlineJudge/model"
-	"net/http"
-	"restweb"
-	"strconv"
+    "GoOnlineJudge/class"
+    // "GoOnlineJudge/config"
+    "GoOnlineJudge/model"
+    "restweb"
+
+    "encoding/json"
+    "net/http"
+    "strconv"
 )
 
 type StatusController struct {
-	class.Controller
-} //@Controller
+    class.Controller
+}   //@Controller
 
-//@URL: /status @method: GET
+//@URL: /api/status @method: GET
 func (sc *StatusController) List() {
-	restweb.Logger.Debug("Status List")
+    restweb.Logger.Debug("Status List")
+    in := struct {
+        Uid      string
+        Pid      string
+        Language int
+        Judge    int
+        Module   int
+        Offset   int
+        Limit    int
+    }{}
 
-	searchUrl := ""
-	qry := make(map[string]string)
-	// Search
-	if v, ok := sc.Input["uid"]; ok {
-		searchUrl += "uid=" + v[0] + "&"
-		sc.Output["SearchUid"] = v[0]
-		qry["uid"] = v[0]
-	}
-	if v, ok := sc.Input["pid"]; ok {
-		searchUrl += "pid=" + v[0] + "&"
-		sc.Output["SearchPid"] = v[0]
-		qry["pid"] = v[0]
-	}
-	if v, ok := sc.Input["judge"]; ok {
-		searchUrl += "judge=" + v[0] + "&"
-		sc.Output["SearchJudge"+v[0]] = v[0]
-		qry["judge"] = v[0]
-	}
-	if v, ok := sc.Input["language"]; ok {
-		searchUrl += "language=" + v[0] + "&"
-		sc.Output["SearchLanguage"+v[0]] = v[0]
-		qry["language"] = v[0]
-	}
-	sc.Output["URL"] = "/status?" + searchUrl
+    if err := json.NewDecoder(sc.R.Body).Decode(&in); err != nil {
+        sc.Error(err.Error(), http.StatusBadRequest)
+        return
+    }
+    qry := make(map[string]string)
+    solutionModel := &model.SolutionModel{}
 
-	// Page
-	qry["page"] = "1"
+    list, err := solutionModel.List(qry)
+    if err != nil {
+        sc.Error(err.Error(), 500)
+        return
+    }
 
-	if v, ok := sc.Input["page"]; ok {
-		qry["page"] = v[0]
-	}
-
-	solutionModel := model.SolutionModel{}
-	qry["module"] = strconv.Itoa(config.ModuleP)
-	qry["action"] = "submit"
-	count, err := solutionModel.Count(qry)
-	if err != nil {
-		sc.Error(err.Error(), 400)
-		return
-	}
-	var pageCount = (count-1)/config.SolutionPerPage + 1
-
-	page, err := strconv.Atoi(qry["page"])
-	if err != nil {
-		sc.Error("args error", 400)
-		return
-	}
-	if page > pageCount {
-		sc.Error("args error", 400)
-		return
-	}
-	qry["offset"] = strconv.Itoa((page - 1) * config.SolutionPerPage)
-	qry["limit"] = strconv.Itoa(config.SolutionPerPage)
-
-	pageData := sc.GetPage(page, pageCount)
-	for k, v := range pageData {
-		sc.Output[k] = v
-	}
-
-	list, err := solutionModel.List(qry)
-	if err != nil {
-		sc.Error(err.Error(), 500)
-		return
-	}
-
-	sc.Output["Solution"] = list
-	sc.Output["Title"] = "Status List"
-	sc.Output["IsStatus"] = true
-	sc.Output["Privilege"] = sc.Privilege
-	sc.Output["Uid"] = sc.Uid
-
-	sc.RenderTemplate("view/layout.tpl", "view/status_list.tpl")
+    sc.Output["Solution"] = list
+    sc.RenderJson()
 }
 
-//@URL: /status/code @method: GET
+//@URL: /api/status/code @method: GET
 func (sc *StatusController) Code() {
-	restweb.Logger.Debug("Status Code")
+    restweb.Logger.Debug("Status Code")
 
-	sid, err := strconv.Atoi(sc.Input.Get("sid"))
-	if err != nil {
-		http.Error(sc.W, "args error", 400)
-		return
-	}
+    sid, err := strconv.Atoi(sc.Input.Get("sid"))
+    if err != nil {
+        http.Error(sc.W, "args error", 400)
+        return
+    }
 
-	solutionModel := model.SolutionModel{}
-	one, err := solutionModel.Detail(sid)
-	if err != nil {
-		http.Error(sc.W, err.Error(), 400)
-		return
-	}
-	if one.Error != "" {
-		one.Code = one.Code + "\n/*\n" + one.Error + "*/\n"
-	}
+    solutionModel := model.SolutionModel{}
+    one, err := solutionModel.Detail(sid)
+    if err != nil {
+        sc.Error(err.Error(), 400)
+        return
+    }
+    if one.Error != "" {
+        one.Code = one.Code + "\n/*\n" + one.Error + "*/\n"
+    }
 
-	if one.Uid == sc.Uid || sc.Privilege > config.PrivilegePU || one.Share {
-		sc.Output["Solution"] = one
-		sc.Output["Title"] = "View Code"
-		sc.Output["IsCode"] = true
-		sc.RenderTemplate("view/layout.tpl", "view/status_code.tpl")
-	} else {
-		sc.Err400("Warning", "You can't see it!")
-	}
+    sc.Output["Solution"] = one
+    sc.RenderJson()
 }
