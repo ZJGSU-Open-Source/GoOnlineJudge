e3ae3617d8df3ea805662e063c9ab08a5f1e5b30
JinweiClarkChao <clarkzjw@gmail.com>
2015-05-22 00:02:41 +0800

    fixed sim test

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	_ "GoOnlineJudge/schedule"
 	"log"
 	"restweb"
 
@@ -13,28 +12,28 @@ import (
 func main() {
 
 	restweb.RegisterController(&controller.NewsController{})
-	restweb.RegisterController(&controller.OSCController{})
-	restweb.RegisterController(&controller.SessController{})
-	restweb.RegisterController(&controller.ContestController{})
-	restweb.RegisterController(&controller.FAQController{})
-	restweb.RegisterController(&controller.StatusController{})
-	restweb.RegisterController(&controller.UserController{})
-	restweb.RegisterController(&controller.HomeController{})
 	restweb.RegisterController(&controller.ProblemController{})
+	restweb.RegisterController(&controller.UserController{})
+	restweb.RegisterController(&controller.OSCController{})
+	restweb.RegisterController(&controller.HomeController{})
+	restweb.RegisterController(&controller.StatusController{})
+	restweb.RegisterController(&controller.ContestController{})
 	restweb.RegisterController(&controller.RanklistController{})
-	restweb.RegisterController(&admin.AdminRejudge{})
+	restweb.RegisterController(&controller.FAQController{})
+	restweb.RegisterController(&controller.SessController{})
 	restweb.RegisterController(&admin.AdminTestdata{})
+	restweb.RegisterController(&admin.AdminNews{})
 	restweb.RegisterController(&admin.AdminProblem{})
 	restweb.RegisterController(&admin.AdminUser{})
-	restweb.RegisterController(&admin.AdminContest{})
-	restweb.RegisterController(&admin.AdminHome{})
 	restweb.RegisterController(&admin.AdminImage{})
-	restweb.RegisterController(&admin.AdminNews{})
+	restweb.RegisterController(&admin.AdminHome{})
+	restweb.RegisterController(&admin.AdminContest{})
+	restweb.RegisterController(&admin.AdminRejudge{})
 	restweb.RegisterController(&admin.AdminNotice{})
-	restweb.RegisterController(&contest.ContestProblem{})
-	restweb.RegisterController(&contest.ContestRanklist{})
 	restweb.RegisterController(&contest.ContestStatus{})
+	restweb.RegisterController(&contest.ContestProblem{})
 	restweb.RegisterController(&contest.Contest{})
+	restweb.RegisterController(&contest.ContestRanklist{})
 
 	restweb.AddFile("/static/", ".")
 	log.Fatal(restweb.Run())
