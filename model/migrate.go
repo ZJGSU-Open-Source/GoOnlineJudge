cd7323d49b342d90986dc24c23b4ba4422a45b63
sakeven <jc5930@sina.cn>
2015-07-09 00:26:16 +0800

    add migration

---
+++ model/migrate.go
@@ -0,0 +1,40 @@
+package model
+
+func init() {
+	setup()
+}
+
+type Migrator func()
+
+func setup() {
+	var Migrations = []Migrator{
+		Migrate_2015_07_09,
+	}
+
+	mgt := &migrateModel{}
+	mi, err := mgt.Get()
+	if err != nil {
+		return
+	}
+
+	if len(Migrations) < mi.Version {
+		return
+	}
+
+	for _, m := range Migrations[mi.Version:] {
+		m()
+	}
+
+	mgt.Update(len(Migrations))
+
+}
+
+func Migrate_2015_07_09() {
+	userModel := &UserModel{}
+	user := &User{}
+	user.Uid = "admin"
+	user.Pwd = "admin"
+	user.Nick = "admin"
+
+	userModel.Insert(*user)
+}
