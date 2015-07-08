6371a0e91d8a8e89aa1a630125c7ca8cdd9bb48f
sakeven <jc5930@sina.cn>
2015-07-08 17:40:37 +0800

    mongo config

--- model/class/model.go
+++ model/class/model.go
@@ -4,14 +4,14 @@ import (
 	"crypto/md5"
 	"crypto/sha1"
 	"fmt"
+	"io"
+	"os"
+	"time"
+
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
-	"io"
-	"time"
 )
 
-const DBHost = "127.0.0.1"
-const DBName = "oj"
 const DBLasting = false
 
 type ids struct {
@@ -22,16 +22,6 @@ type ids struct {
 type Model struct {
 	Session *mgo.Session
 	DB      *mgo.Database
-}
-
-func (this *Model) OpenDB() (err error) {
-	this.Session, err = mgo.Dial(DBHost)
-	if err != nil {
-		return
-	}
-
-	this.DB = this.Session.DB(DBName)
-	return
 }
 
 func (this *Model) CloseDB() {
@@ -68,4 +58,55 @@ func (this *Model) EncryptPassword(str string) (pwd string, err error) {
 	p2 := fmt.Sprintf("%x", s.Sum(nil))
 	pwd = p1 + p2
 	return
+}
+
+var (
+	username string
+	password string
+	host     string
+	port     string
+	instance string
+)
+
+func Config() {
+
+	username = os.Getenv("MONGODB_USERNAME")
+	password = os.Getenv("MONGODB_PASSWORD")
+	host = os.Getenv("MONGODB_PORT_27017_TCP_ADDR")
+
+	if len(host) == 0 {
+		host = "localhost"
+	}
+
+	port = os.Getenv("MONGODB_PORT_27017_TCP_PORT")
+	if len(port) == 0 {
+		port = "27017"
+	}
+
+	instance = os.Getenv("MONGODB_INSTANCE_NAME")
+}
+
+func (m *Model) OpenDB() error {
+	conn := ""
+	if len(username) > 0 {
+		conn += username
+
+		if len(password) > 0 {
+			conn += ":" + password
+		}
+
+		conn += "@"
+	}
+
+	conn += fmt.Sprintf("%s:%s/%s", host, port, instance)
+
+	var err error
+
+	m.Session, err = mgo.Dial(conn)
+	if err != nil {
+		return err
+	}
+
+	m.DB = m.Session.DB(instance)
+	return nil
 }
