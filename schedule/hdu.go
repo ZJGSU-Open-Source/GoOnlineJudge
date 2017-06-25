5979347cc59cf40f1b5f3752655972396078cdac
Sakeven Jiang <sakeven@users.noreply.github.com>
2017-06-25 14:16:27 +0800

    Update hdu.go

--- schedule/hdu.go
+++ schedule/hdu.go
@@ -1,199 +1,199 @@
 package schedule
 
-import (
-	"GoOnlineJudge/model"
-	"html/template"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"os"
-	"regexp"
-	"strconv"
-	"strings"
-	"time"
+// import (
+// 	"GoOnlineJudge/model"
+// 	"html/template"
+// 	"io/ioutil"
+// 	"log"
+// 	"net/http"
+// 	"os"
+// 	"regexp"
+// 	"strconv"
+// 	"strings"
+// 	"time"
 
-	iconv "github.com/djimenez/iconv-go"
-)
+// 	iconv "github.com/djimenez/iconv-go"
+// )
 
-type HDUJudger struct {
-	client    *http.Client
-	testRx    *regexp.Regexp
-	titleRx   *regexp.Regexp
-	resLimtRx *regexp.Regexp
-	ctxRx     *regexp.Regexp
-	srcRx     *regexp.Regexp
-	hintRx    *regexp.Regexp
-}
+// type HDUJudger struct {
+// 	client    *http.Client
+// 	testRx    *regexp.Regexp
+// 	titleRx   *regexp.Regexp
+// 	resLimtRx *regexp.Regexp
+// 	ctxRx     *regexp.Regexp
+// 	srcRx     *regexp.Regexp
+// 	hintRx    *regexp.Regexp
+// }
 
-var hdulogger *log.Logger
+// var hdulogger *log.Logger
 
-func (h *HDUJudger) Host() string {
-	return "HDU"
-}
+// func (h *HDUJudger) Host() string {
+// 	return "HDU"
+// }
 
-func (h *HDUJudger) Ping() error {
-	h.client = &http.Client{Timeout: time.Second * 10}
-	resp, err := h.client.Get("http://acm.hdu.edu.cn/showproblem.php?pid=1000")
-	if err != nil {
-		return ErrConnectFailed
-	}
-	if resp.StatusCode >= 200 && resp.StatusCode < 300 {
-		return nil
-	}
-	return ErrResponse
-}
+// func (h *HDUJudger) Ping() error {
+// 	h.client = &http.Client{Timeout: time.Second * 10}
+// 	resp, err := h.client.Get("http://acm.hdu.edu.cn/showproblem.php?pid=1000")
+// 	if err != nil {
+// 		return ErrConnectFailed
+// 	}
+// 	if resp.StatusCode >= 200 && resp.StatusCode < 300 {
+// 		return nil
+// 	}
+// 	return ErrResponse
+// }
 
-func (h *HDUJudger) Init() {
-	h.client = &http.Client{Timeout: time.Second * 10}
+// func (h *HDUJudger) Init() {
+// 	h.client = &http.Client{Timeout: time.Second * 10}
 
-	titlePat := `<h1 style='color:#1A5CC8'>(.*?)</h1>`
-	h.titleRx = regexp.MustCompile(titlePat)
+// 	titlePat := `<h1 style='color:#1A5CC8'>(.*?)</h1>`
+// 	h.titleRx = regexp.MustCompile(titlePat)
 
-	resLimtPat := `Time Limit: \d+/(\d+) MS \(Java/Others\)&nbsp;&nbsp;&nbsp;&nbsp;Memory Limit: \d+/(\d+) K \(Java/Others\)<br>`
-	h.resLimtRx = regexp.MustCompile(resLimtPat)
+// 	resLimtPat := `Time Limit: \d+/(\d+) MS \(Java/Others\)&nbsp;&nbsp;&nbsp;&nbsp;Memory Limit: \d+/(\d+) K \(Java/Others\)<br>`
+// 	h.resLimtRx = regexp.MustCompile(resLimtPat)
 
-	ctxPat := `(?s)<div class=panel_content>(.*?)</div><div class=panel_bottom>`
-	h.ctxRx = regexp.MustCompile(ctxPat)
+// 	ctxPat := `(?s)<div class=panel_content>(.*?)</div><div class=panel_bottom>`
+// 	h.ctxRx = regexp.MustCompile(ctxPat)
 
-	testPat := `(?s)<div style="font-family:Courier New,Courier,monospace;">(.*?)</??div`
-	h.testRx = regexp.MustCompile(testPat)
+// 	testPat := `(?s)<div style="font-family:Courier New,Courier,monospace;">(.*?)</??div`
+// 	h.testRx = regexp.MustCompile(testPat)
 
-	srcPat := `<a href="/search.php\?field=problem&key=.*?&source=1&searchmode=source"> (.*?) </a>`
-	h.srcRx = regexp.MustCompile(srcPat)
+// 	srcPat := `<a href="/search.php\?field=problem&key=.*?&source=1&searchmode=source"> (.*?) </a>`
+// 	h.srcRx = regexp.MustCompile(srcPat)
 
-	hintPat := `(?s)<i>Hint</i></div>(.*?)</div>`
-	h.hintRx = regexp.MustCompile(hintPat)
+// 	hintPat := `(?s)<i>Hint</i></div>(.*?)</div>`
+// 	h.hintRx = regexp.MustCompile(hintPat)
 
-	hduLogfile, err := os.Create("log/hdu.log")
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	hdulogger = log.New(hduLogfile, "[Hdu]", log.Ldate|log.Ltime)
-}
+// 	hduLogfile, err := os.Create("log/hdu.log")
+// 	if err != nil {
+// 		log.Println(err)
+// 		return
+// 	}
+// 	hdulogger = log.New(hduLogfile, "[Hdu]", log.Ldate|log.Ltime)
+// }
 
-func (h *HDUJudger) GetProblemPage(pid string) (string, error) {
-	resp, err := h.client.Get("http://acm.hdu.edu.cn/showproblem.php?pid=" + pid)
-	if err != nil {
-		return "", ErrConnectFailed
-	}
-	b, _ := ioutil.ReadAll(resp.Body)
-	html := string(b)
-	return html, nil
+// func (h *HDUJudger) GetProblemPage(pid string) (string, error) {
+// 	resp, err := h.client.Get("http://acm.hdu.edu.cn/showproblem.php?pid=" + pid)
+// 	if err != nil {
+// 		return "", ErrConnectFailed
+// 	}
+// 	b, _ := ioutil.ReadAll(resp.Body)
+// 	html := string(b)
+// 	return html, nil
 
-}
-func (h *HDUJudger) IsExist(page string) bool {
-	return strings.Index(page, "No such problem") < 0
-}
-func (h *HDUJudger) ReplaceImg(text string) string {
-	text = strings.Replace(text, `<img src=/data/images/`, `<img src=http://acm.hdu.edu.cn/data/images/`, -1)
-	text = strings.Replace(text, `<img src=data/images/`, `<img src=http://acm.hdu.edu.cn/data/images/`, -1)
-	text = strings.Replace(text, `<img src=../../../data/images/`, `<img src=http://acm.hdu.edu.cn/data/images/`, -1)
-	text = strings.Replace(text, `<img src=../../data/images/`, `<img src=http://acm.hdu.edu.cn/data/images/`, -1)
-	return text
-}
+// }
+// func (h *HDUJudger) IsExist(page string) bool {
+// 	return strings.Index(page, "No such problem") < 0
+// }
+// func (h *HDUJudger) ReplaceImg(text string) string {
+// 	text = strings.Replace(text, `<img src=/data/images/`, `<img src=http://acm.hdu.edu.cn/data/images/`, -1)
+// 	text = strings.Replace(text, `<img src=data/images/`, `<img src=http://acm.hdu.edu.cn/data/images/`, -1)
+// 	text = strings.Replace(text, `<img src=../../../data/images/`, `<img src=http://acm.hdu.edu.cn/data/images/`, -1)
+// 	text = strings.Replace(text, `<img src=../../data/images/`, `<img src=http://acm.hdu.edu.cn/data/images/`, -1)
+// 	return text
+// }
 
-func (h *HDUJudger) SetDetail(pid string, html string) error {
-	log.Println(pid)
-	pro := model.Problem{}
-	pro.RPid, _ = strconv.Atoi(pid)
-	pro.ROJ = "HDU"
-	pro.Status = StatusAvailable
+// func (h *HDUJudger) SetDetail(pid string, html string) error {
+// 	log.Println(pid)
+// 	pro := model.Problem{}
+// 	pro.RPid, _ = strconv.Atoi(pid)
+// 	pro.ROJ = "HDU"
+// 	pro.Status = StatusAvailable
 
-	titleMatch := h.titleRx.FindStringSubmatch(html)
-	if len(titleMatch) < 1 {
-		log.Println(titleMatch)
-		return ErrMatchFailed
-	}
-	pro.Title = titleMatch[1]
+// 	titleMatch := h.titleRx.FindStringSubmatch(html)
+// 	if len(titleMatch) < 1 {
+// 		log.Println(titleMatch)
+// 		return ErrMatchFailed
+// 	}
+// 	pro.Title = titleMatch[1]
 
-	if strings.Index(html, "Special Judge") >= 0 {
-		pro.Special = 1
-	}
+// 	if strings.Index(html, "Special Judge") >= 0 {
+// 		pro.Special = 1
+// 	}
 
-	resMatch := h.resLimtRx.FindStringSubmatch(html)
-	if len(resMatch) < 3 {
-		log.Println(resMatch)
-		return ErrMatchFailed
-	}
-	pro.Time, _ = strconv.Atoi(resMatch[1])
-	pro.Time /= 1000 //ms -> s
-	pro.Memory, _ = strconv.Atoi(resMatch[2])
+// 	resMatch := h.resLimtRx.FindStringSubmatch(html)
+// 	if len(resMatch) < 3 {
+// 		log.Println(resMatch)
+// 		return ErrMatchFailed
+// 	}
+// 	pro.Time, _ = strconv.Atoi(resMatch[1])
+// 	pro.Time /= 1000 //ms -> s
+// 	pro.Memory, _ = strconv.Atoi(resMatch[2])
 
-	cxtMatch := h.ctxRx.FindAllStringSubmatch(html, 3)
-	if len(cxtMatch) < 3 {
-		log.Println("ctx match error, hdu pid is", pid)
-		return ErrMatchFailed
-	}
-	pro.Description = template.HTML(h.ReplaceImg(cxtMatch[0][1]))
-	pro.Input = template.HTML(h.ReplaceImg(cxtMatch[1][1]))
-	pro.Output = template.HTML(h.ReplaceImg(cxtMatch[2][1]))
+// 	cxtMatch := h.ctxRx.FindAllStringSubmatch(html, 3)
+// 	if len(cxtMatch) < 3 {
+// 		log.Println("ctx match error, hdu pid is", pid)
+// 		return ErrMatchFailed
+// 	}
+// 	pro.Description = template.HTML(h.ReplaceImg(cxtMatch[0][1]))
+// 	pro.Input = template.HTML(h.ReplaceImg(cxtMatch[1][1]))
+// 	pro.Output = template.HTML(h.ReplaceImg(cxtMatch[2][1]))
 
-	test := h.testRx.FindAllStringSubmatch(html, 2)
-	if len(test) < 2 {
-		log.Println("test data error, hdu pid is", pid)
-		return ErrMatchFailed
-	}
-	pro.In = test[0][1]
-	pro.Out = test[1][1]
+// 	test := h.testRx.FindAllStringSubmatch(html, 2)
+// 	if len(test) < 2 {
+// 		log.Println("test data error, hdu pid is", pid)
+// 		return ErrMatchFailed
+// 	}
+// 	pro.In = test[0][1]
+// 	pro.Out = test[1][1]
 
-	src := h.srcRx.FindStringSubmatch(html)
-	if len(src) > 1 {
-		pro.Source = src[1]
-	}
+// 	src := h.srcRx.FindStringSubmatch(html)
+// 	if len(src) > 1 {
+// 		pro.Source = src[1]
+// 	}
 
-	hint := h.hintRx.FindStringSubmatch(html)
-	if len(hint) > 1 {
-		pro.Hint = template.HTML(hint[1])
-	}
+// 	hint := h.hintRx.FindStringSubmatch(html)
+// 	if len(hint) > 1 {
+// 		pro.Hint = template.HTML(hint[1])
+// 	}
 
-	proModel := &model.ProblemModel{}
-	proModel.Insert(pro)
-	return nil
-}
+// 	proModel := &model.ProblemModel{}
+// 	proModel.Insert(pro)
+// 	return nil
+// }
 
-func (h *HDUJudger) GetProblems() error {
-	vidsModel := &model.VIdsModel{}
-	StartId, err := vidsModel.GetLastID("HDU")
-	if err == model.DBErr {
-		return err
-	} else if StartId < 1000 {
-		StartId = 999
-	}
-	errCnt := 0
-	lastId := StartId
-	for i := 1; ; i++ {
-		pid := strconv.Itoa(StartId + i)
-		page, err := h.GetProblemPage(pid)
-		if err != nil { //offline
-			hdulogger.Println("pid["+pid+"]: ", err, ".")
-			return err
-		}
-		cpage, err := iconv.ConvertString(page, "gb2312", "utf-8")
-		if err != nil { //Although getting error, continue proccess it.
-			hdulogger.Println("pid["+pid+"]: ", "encode convert error.")
-			cpage = page
-		}
+// func (h *HDUJudger) GetProblems() error {
+// 	vidsModel := &model.VIdsModel{}
+// 	StartId, err := vidsModel.GetLastID("HDU")
+// 	if err == model.DBErr {
+// 		return err
+// 	} else if StartId < 1000 {
+// 		StartId = 999
+// 	}
+// 	errCnt := 0
+// 	lastId := StartId
+// 	for i := 1; ; i++ {
+// 		pid := strconv.Itoa(StartId + i)
+// 		page, err := h.GetProblemPage(pid)
+// 		if err != nil { //offline
+// 			hdulogger.Println("pid["+pid+"]: ", err, ".")
+// 			return err
+// 		}
+// 		cpage, err := iconv.ConvertString(page, "gb2312", "utf-8")
+// 		if err != nil { //Although getting error, continue proccess it.
+// 			hdulogger.Println("pid["+pid+"]: ", "encode convert error.")
+// 			cpage = page
+// 		}
 
-		if h.IsExist(cpage) {
-			err := h.SetDetail(pid, cpage)
-			if err != nil {
-				hdulogger.Println("pid["+pid+"]: ", "import error.")
-			} else {
-				lastId = StartId + i
-				vidsModel.SetLastID("HDU", lastId)
-			}
-			errCnt = 0
-		} else {
-			hdulogger.Println("pid["+pid+"]: ", "not exist.")
-			errCnt++
-		}
+// 		if h.IsExist(cpage) {
+// 			err := h.SetDetail(pid, cpage)
+// 			if err != nil {
+// 				hdulogger.Println("pid["+pid+"]: ", "import error.")
+// 			} else {
+// 				lastId = StartId + i
+// 				vidsModel.SetLastID("HDU", lastId)
+// 			}
+// 			errCnt = 0
+// 		} else {
+// 			hdulogger.Println("pid["+pid+"]: ", "not exist.")
+// 			errCnt++
+// 		}
 
-		if errCnt >= 100 { //If "not exist" continuously repeat 100 times, terminate it.
-			break
-		}
-	}
+// 		if errCnt >= 100 { //If "not exist" continuously repeat 100 times, terminate it.
+// 			break
+// 		}
+// 	}
 
-	hdulogger.Println("import terminated. Last pid is ", lastId, ".")
-	return nil
-}
+// 	hdulogger.Println("import terminated. Last pid is ", lastId, ".")
+// 	return nil
+// }
